606e29fd7fce7e554659a37e460baf613526f6f0
Kaviraj Jagadeesan <kavirajjdsn@gmail.com>
2025-09-27 12:32:24 +0530

    CH1 L5 - Channel Refactor

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bytes"
 	"fmt"
+	"io"
 	"log"
 	"os"
 )
@@ -12,21 +13,36 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	line := ""
-	for {
-		data := make([]byte, 8)
-		n, err := file.Read(data)
-		if err != nil {
-			break
-		}
-		data = data[:n]
-		if idx := bytes.IndexByte(data, '\n'); idx != -1 {
-			line += string(data[:idx])
-			data = data[idx+1:]
-			fmt.Printf("read: %s\n", line)
-			line = ""
-		}
-		line += string(data)
+	lines := getLinesChannel(file)
 
+	for line := range lines {
+		fmt.Printf("read: %s\n", line)
 	}
+}
+
+func getLinesChannel(file io.ReadCloser) <-chan string {
+	out := make(chan string)
+	go func() {
+		line := ""
+		defer file.Close()
+		defer close(out)
+		for {
+
+			data := make([]byte, 8)
+			n, err := file.Read(data)
+			if err != nil {
+				break
+			}
+			data = data[:n]
+			if idx := bytes.IndexByte(data, '\n'); idx != -1 {
+				line += string(data[:idx])
+				data = data[idx+1:]
+				out <- line
+				line = ""
+			}
+			line += string(data)
+
+		}
+	}()
+	return out
 }
